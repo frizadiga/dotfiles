c1c5e399d260db7df3d513d27cc3b4cf564494ce
frizadiga <frizadiga@gmail.com>
2024-10-22 20:02:29 +0700

    sync: batch

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -5,20 +5,29 @@ return {
   config = function()
     -- calling `setup` is optional for customization
     require("fzf-lua").setup({
-      winopts = {
-				preview = {
-					border = "rounded",
-					style = "minimal",
-					position = "bottom",
-					-- position = "right",
-					layout = "vertical",
-				},
-			},
+			 winopts = {
+          -- Window layout
+          height     = 0.8,
+          width      = 80,
+          preview    = {
+            vertical   = 'up:65%',   -- Preview window on top
+            horizontal = 'right:50%', -- For horizontal preview
+            layout     = 'vertical',
+            flip_columns = 120,
+          },
+          -- Position the results in the middle
+          row     = 0.35,
+          col     = 0.50,
+          -- Border style
+          border     = 'rounded',
+          fullscreen = false,
+        },
 		})
 
-		-- :lua require('fzf-lua').files({ fzf_opts = {['--layout'] = 'reverse-list'} })
+		-- navigate on result using <CTRL> + hjkl keys
+
 		vim.api.nvim_set_keymap('n', '<leader>fz',
-			"<cmd>lua require('fzf-lua').files({ fzf_opts = {['--layout'] = 'reverse-list'} })<CR>",
+			":lua require('fzf-lua').files({ fzf_opts = {['--layout'] = 'reverse-list'} })<CR>",
 			{ noremap = true, silent = true }
 		)
   end
