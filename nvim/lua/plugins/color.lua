cb86c9e22c637428f6c1bcbb554a95ec6bbaa0f9
frizadiga <frizadiga@gmail.com>
2024-11-06 17:55:27 +0700

    sync: batch

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -7,10 +7,10 @@ return {
   priority = 1000,
   config = function()
     require('kanagawa').setup({
-			-- " 1. Modify your config
-			-- " 2. Restart nvim
-			-- " 3. Run this command:
-			-- :KanagawaCompile
+      -- " 1. Modify your config
+      -- " 2. Restart nvim
+      -- " 3. Run this command:
+      -- :KanagawaCompile
       compile = true,  -- enable compiling the colorscheme
       undercurl = true,  -- enable undercurls
       commentStyle = { italic = false },
@@ -22,38 +22,38 @@ return {
       dimInactive = false,  -- dim inactive window `:h hl-NormalNC`
       terminalColors = true,  -- define vim.g.terminal_color_{0,17}
       colors = {  -- add/modify theme and palette colors
-          palette = {},
-          theme = {
-            wave = {},
-            lotus = {},
-            dragon = {},
-            all = {
-              ui = { bg_gutter = "none", float = { bg = "none" } }
-            }
-          },
+        palette = {},
+        theme = {
+          wave = {},
+          lotus = {},
+          dragon = {},
+          all = {
+            ui = { bg_gutter = "none", float = { bg = "none" } }
+          }
+        },
       },
       overrides = function() -- add/modify highlights
-      -- overrides = function(colors) -- add/modify highlights
-				-- return {
-				--   -- telescope border
-				--   TelescopeBorder = { fg = "none", bg = "none" },
-				-- }
+        -- overrides = function(colors) -- add/modify highlights
+        -- return {
+        --   -- telescope border
+        --   TelescopeBorder = { fg = "none", bg = "none" },
+        -- }
         -- local theme = colors.theme
         return {
-            NormalFloat = { bg = "none" },
-            FloatBorder = { bg = "none" },
-            FloatTitle = { bg = "none" },
-            TelescopeBorder = { fg = "none", bg = "none" },
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          TelescopeBorder = { fg = "none", bg = "none" },
 
-            -- Save an hlgroup with dark background and dimmed foreground
-            -- so that you can use it where your still want darker windows.
-            -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-            -- NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+          -- Save an hlgroup with dark background and dimmed foreground
+          -- so that you can use it where your still want darker windows.
+          -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+          -- NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-            -- Popular plugins that open floats will link to NormalFloat by default;
-            -- set their background accordingly if you wish to keep them dark and borderless
-            -- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-            -- MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          -- Popular plugins that open floats will link to NormalFloat by default;
+          -- set their background accordingly if you wish to keep them dark and borderless
+          -- LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+          -- MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
         }
       end,
       -- theme = "dragon",              -- Load "wave" theme when 'background' option is not set
@@ -61,10 +61,10 @@ return {
       --     dark = "dragon",           -- try "dragon" !
       --     light = "lotus"
       -- },
-  })
+    })
 
-  -- setup must be called before loading
-  vim.cmd("colorscheme kanagawa-dragon")
+    -- setup must be called before loading
+    vim.cmd("colorscheme kanagawa-dragon")
   end
 }
 
