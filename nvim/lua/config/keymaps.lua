0d40cd8052b7089c8533f4c9a97205959af340e2
frizadiga <frizadiga@gmail.com>
2024-11-10 00:15:04 +0700

    sync: batch

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,42 +1,38 @@
--- global keymaps
+-- generic keymaps
+-- see: https://neovim.io/doc/user/map.html or `:help map`
 
--- add workspace folder
--- vim.keymap.set("n", "<leader>awf", vim.lsp.buf.add_workspace_folder)
-
--- esc composite
-vim.keymap.set("i", "kj", "<Esc>", {})
-vim.keymap.set("v", "KJ", "<Esc>", {})
+-- composite escape
+vim.keymap.set('i', 'kj', '<Esc>')
+vim.keymap.set('v', 'KJ', '<Esc>')
 
 -- quit
-vim.keymap.set("n", "<leader>qq", "<CMD>q<CR>")
-
--- copy to clipboard current file path
-vim.keymap.set(
-  "n",
-  "<leader>cp",
-  "<CMD>let @+=expand('%:p')<CR>:echo 'Copied: ' . expand('%:p')<CR>"
-)
+vim.keymap.set('n', '<leader>qq', '<CMD>q<CR>')
 
 -- write
-vim.keymap.set("n", "<Space><Space>", "<CMD>w<CR>")
+vim.keymap.set('n', '<Space><Space>', '<CMD>w<CR>')
 
--- replace
-vim.keymap.set("n", "<leader>r", ":%s//g<Left><Left>")
--- in visual mode prefill search with selected text
-vim.keymap.set("v", "<leader>r", ":s/<C-r><C-w>//g<Left><Left>")
-
+-- find
 -- copy selected text to search input when pressing `/`
 vim.keymap.set('v', '/', [[y/\V<C-R>=escape(@", '/\')<CR><CR>]])
 
--- start indentation
+-- replace
+vim.keymap.set('n', '<leader>r', ':%s//g<Left><Left>')
+-- in visual mode prefill search with selected text
+vim.keymap.set('v', '<leader>r', ':s/<C-r><C-w>//g<Left><Left>')
+
+-- copy to clipboard current file path
+vim.keymap.set(
+  'n',
+  '<leader>cp',
+  '<CMD>let @+=expand("%:p")<CR>:echo "Copied: " .. expand("%:p")<CR>'
+)
+
+-- @start indentation
 -- basic indentation
-vim.keymap.set('v', '<', '<gv')      -- Indent left in visual mode and stay in visual mode
-vim.keymap.set('v', '>', '>gv')      -- Indent right in visual mode and stay in visual mode
 vim.keymap.set('n', '<Tab>', '>>')   -- Indent right in normal mode
 vim.keymap.set('n', '<S-Tab>', '<<') -- Indent left in normal mode
-
--- fix/adjust indentation
-vim.keymap.set('n', '<leader>i', 'gg=G<C-o>', { desc = 'Fix indentation for entire file' })
+vim.keymap.set('v', '<', '<gv')      -- Indent left in visual mode and stay in visual mode
+vim.keymap.set('v', '>', '>gv')      -- Indent right in visual mode and stay in visual mode
 
 -- auto indent when pasting
 vim.keymap.set('n', 'p', ']p', { desc = 'Paste with auto-indent' })
@@ -44,27 +40,24 @@ vim.keymap.set('n', 'P', '[p', { desc = 'Paste with auto-indent (before cursor)'
 
 -- format specific lines
 vim.keymap.set('n', '=', '==', { desc = 'Fix indentation for current line' })
-vim.keymap.set('n', '<leader>il', 'V=', { desc = 'Fix indentation for current line (alternative)' })
 
 -- visual mode specific
 vim.keymap.set('v', '=', '=', { desc = 'Fix indentation for selected lines' })
 
--- optional: auto format on save for specific filetypes
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.py", "*.lua" },
--- 	callback = function()
--- 		vim.lsp.buf.format()
--- 	end,
--- })
+-- toggle between tabs and spaces
+vim.keymap.set('n', '<leader>tt', function()
+  if vim.opt_local.expandtab:get() then
+    vim.opt_local.expandtab = false
+    print('Using tabs')
+  else
+    vim.opt_local.expandtab = true
+    print('Using spaces')
+  end
+end, { desc = 'Toggle tabs/spaces' })
 
--- optional: toggle between tabs and spaces
--- vim.keymap.set('n', '<leader>tt', function()
---   if vim.opt_local.expandtab:get() then
---     vim.opt_local.expandtab = false
---     print('Using tabs')
---   else
---     vim.opt_local.expandtab = true
---     print('Using spaces')
---   end
--- end, { desc = 'Toggle tabs/spaces' })
--- end indentation
+-- fix/adjust indentation for entire file
+vim.keymap.set('n', '<leader>i', 'gg=G<C-o>', { desc = 'Fix indentation for entire file' })
+-- @end indentation
+
+-- add workspace folder
+-- vim.keymap.set('n', '<leader>awf', vim.lsp.buf.add_workspace_folder)
--- nvim/lua/config/vim.lua
+++ nvim/lua/config/vim.lua
@@ -1,11 +1,17 @@
--- main settings
+-- vim options
+-- see: https://neovim.io/doc/user/options.html or `:help options`
 
--- startup screen
+-- @start startup screen
 -- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
 -- disable default intro
 vim.opt.shortmess:append("I")
+-- @end startup screen
+
+-- sign column
+vim.wo.signcolumn = 'yes' -- (why? prevent layout shift)
 
 -- set autoread
 vim.opt.autoread = true
@@ -15,69 +21,56 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "," -- Same for `maplocalleader`
 -- vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
--- cursor
+-- @start cursor
 -- hide GUI cursor
 -- vim.opt.guicursor = ""
 -- change cursor type on diff mode
 -- docs: https://neovim.io/doc/user/options.html#'guicursor'
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:hor100,r-cr-o:hor20"
 -- vim.o.guicursor = 'n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor'
+-- @end cursor
 
 -- show line numbers
-vim.opt.rnu = true
+vim.opt.rnu = true -- relative line numbers
+vim.wo.number = true -- absolute line numbers
 
 -- set fill characters for end of buffer
 vim.opt.fillchars = { eob = ' ' }
 
--- set tabstop and shiftwidth
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-
--- disable highlight search
--- vim.opt.hlsearch = false
-
 vim.opt.timeoutlen = 1000 -- default is 1000
 
--- transparent popup border
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#3B4252" })
-
--- status bar color to transparent
-vim.cmd('hi StatusLine ctermbg=235 cterm=NONE')
-
 -- use system clipboard
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard = 'unnamedplus'
 
 -- set termgui colors
 vim.opt.termguicolors = true
--- vim.api.nvim_set_hl(0, 'NormalFloat', {fg = LightGrey})
--- vim.api.nvim_set_hl(0, "TelescopeBorder", {bg="#ffffff", fg="#ffffff"})
 
+-- @start lsp
 -- show lsp diagnostics on popup
 -- ref: https://stackoverflow.com/a/70760302/6893303
 vim.diagnostic.config({
 	virtual_text = false
 })
+
 -- show line diagnostics automatically in hover window
 vim.o.updatetime = 250
 vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+-- @end lsp
 
--- sign column
-vim.wo.signcolumn = "yes"
-
--- code folding
--- vim.opt.foldmethod = "indent"
--- vim.opt.foldlevel = 99
--- vim.wo.foldmethod = 'expr'
--- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-
--- global indent settings
+-- @start indent
+-- set tabstop and shiftwidth
 vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.shiftwidth = 2   -- Default spaces for auto indent
 vim.opt.tabstop = 2      -- Default spaces for tab
 vim.opt.softtabstop = 2  -- Default spaces when editing
+vim.opt.shiftwidth = 2   -- Default spaces for auto indent
 
--- language specific settings
-vim.api.nvim_create_autocmd("FileType", {
+-- optional: add indent detection for better results
+vim.g.detect_indent = true
+vim.g.detect_indent_max_lines = 1000
+-- @end indent
+
+-- @start language config
+vim.api.nvim_create_autocmd('FileType', {
 	pattern = "python",
 	callback = function()
 		vim.opt_local.expandtab = true
@@ -87,7 +80,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd('FileType', {
 	pattern = "javascript,typescript,javascriptreact,typescriptreact",
 	callback = function()
 		vim.opt_local.expandtab = true
@@ -97,27 +90,27 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd('FileType', {
 	pattern = "go",
 	callback = function()
-		vim.opt_local.expandtab = false -- Use tabs for Go
+		vim.opt_local.expandtab = false
 		vim.opt_local.shiftwidth = 4
 		vim.opt_local.tabstop = 4
 		vim.opt_local.softtabstop = 4
 	end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd('FileType', {
   pattern = "c,cpp",
   callback = function()
-    vim.opt_local.expandtab = false -- Use tabs for C and C++
+    vim.opt_local.expandtab = false
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
     vim.opt_local.softtabstop = 4
   end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd('FileType', {
   pattern = "lua",
   callback = function()
     vim.opt_local.expandtab = true
@@ -128,7 +121,5 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- you can add more languages following the same pattern
+-- @end language config
 
--- optional: add indent detection for better results
-vim.g.detect_indent = true
-vim.g.detect_indent_max_lines = 1000
