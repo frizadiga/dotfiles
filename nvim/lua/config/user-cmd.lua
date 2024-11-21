84d73333cd82b887becf7df8c55e22ceb27b1156
frizadiga <frizadiga@gmail.com>
2024-11-21 18:38:44 +0700

    sync: batch

--- nvim/lua/config/user-cmd.lua
+++ nvim/lua/config/user-cmd.lua
@@ -1,20 +1,50 @@
 -- @start create user commands
+
 -- fn to print reverse lines
 -- local function print_reverse_lines(output)
---   -- Split output into lines
+--   -- split output into lines
 --   local lines = vim.fn.split(output, '\n')
 --
---   -- Print reverse of lines
+--   -- print reverse of lines
 --   for i = #lines, 1, -1 do
 --     print(lines[i])
 --   end
---
---   -- Example usage
---   -- local output = "line1\nline2\nline3"
---   -- print_reverse_lines(output)
 -- end
 
--- Lazy alias
+-- fn to open a floating window
+local function open_floating_window(content_buff, width, height)
+  local width = width or 0.6  -- default width value
+  local height = height or 0.6  -- default height value
+  
+  -- split the command output into lines
+  local buffer_lines = {}
+  for line in content_buff:gmatch("[^\r\n]+") do
+    table.insert(buffer_lines, line)
+  end
+
+  -- create a new buffer and window
+  local buf = vim.api.nvim_create_buf(false, true) -- nofile, scratch buffer
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, buffer_lines)
+  
+  -- define floating window dimensions
+  local _width = math.floor(vim.o.columns * width)
+  local _height = math.floor(vim.o.lines * height)
+  local row = math.floor((vim.o.lines - _height) / 2)
+  local col = math.floor((vim.o.columns - _width) / 2)
+
+  -- open the floating window
+  vim.api.nvim_open_win(buf, true, {
+    relative = "editor",
+    width = _width,
+    height = _height,
+    row = row,
+    col = col,
+    style = "minimal",
+    border = "rounded",
+  })
+end
+
+-- lazy.nvim dashboard alias
 vim.api.nvim_create_user_command('L',
   function(opts)
     vim.cmd('Lazy ' .. opts.args)
@@ -32,7 +62,7 @@ vim.api.nvim_create_user_command(
     local tools_dir = vim.fn.expand('$TOOLS_DIR')
     local output = vim.fn.system({'bash', tools_dir .. '/todo.sh'})
 
-    vim.notify('\n' .. output)
+    open_floating_window(output)
   end,
   {}
 )
@@ -65,8 +95,9 @@ vim.api.nvim_create_user_command(
     local tools_dir = vim.fn.expand('$TOOLS_DIR')
     local output = vim.fn.system({'bash', tools_dir .. '/jira-curl/curl-issue-find-one.sh'})
 
-    vim.notify('\n' .. output)
+    open_floating_window(output)
   end,
   {}
 )
+
 -- @end create user commands
